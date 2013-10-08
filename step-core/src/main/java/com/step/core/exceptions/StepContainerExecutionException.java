31429a942810689b3731130e06a329dae9a3403c
Ashish <vikasmishra82@gmail.com>
2013-10-08 16:58:21 +0530

    added missing file

---
+++ step-core/src/main/java/com/step/core/exceptions/StepContainerExecutionException.java
@@ -0,0 +1,14 @@
+package com.step.core.exceptions;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: amishra
+ * Date: 10/7/13
+ * Time: 4:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StepContainerExecutionException extends RuntimeException{
+     public StepContainerExecutionException(String message){
+         super(message);
+     }
+}
